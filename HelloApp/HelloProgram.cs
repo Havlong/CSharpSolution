d2f79e08da5bfb66a1cee8fc17f78b2ead533595
Havlong <grishin_evgenii@inbox.ru>
2020-02-22 12:11:39 +0300

    Initial commit

---
+++ HelloApp/HelloProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloApp
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HelloApp/Student.cs
@@ -0,0 +1,7 @@
+namespace HelloApp
+{
+    public class Student
+    {
+        
+    }
+}
